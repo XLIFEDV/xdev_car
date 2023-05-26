dd947233807a5b13c6cf91233b9a9e5fd03e1129
XDEVELOPMENT <121575497+XLIFEDV@users.noreply.github.com>
2023-05-27 02:24:43 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,17 +9,8 @@ files {
     'ayar/**/*.meta',
 }
 
-data_file 'HANDLING_FILE' 'data/**/**/handling.meta'
-data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/**/vehiclelayouts_formula.meta'
-data_file 'VEHICLE_METADATA_FILE' 'data/**/**/vehicles.meta'
-data_file 'VEHICLE_VARIATION_FILE' 'data/**/**/carvariations.meta'
-data_file 'CARCOLS_FILE' 'data/**/**/carcols.meta'
-data_file 'HANDLING_FILE' 'data/**/handling.meta'
-data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
-data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
-data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
-data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
-data_file 'VEHICLE_LAYOUTS_FILE' 'data/f1/vehiclelayouts_formula.meta'
-data_file 'CONTENT_UNLOCKING_META_FILE' 'data/f22a/contentunlocks.meta'
-data_file 'WEAPONINFO_FILE' 'dosya/f22a/f22aweapons.meta'
-data_file 'WEAPON_METADATA_FILE' 'dosya/f22a/weaponarchetypes.meta'
+data_file 'HANDLING_FILE' 'ayar/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'ayar/**/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'ayar/**/carvariations.meta'
+data_file 'CARCOLS_FILE' 'ayar/**/carcols.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'ayar/**/vehiclelayouts.meta'

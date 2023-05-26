7bd92370b9cc11cea08f27c7769beb3618705290
XDEVELOPMENT <121575497+XLIFEDV@users.noreply.github.com>
2023-05-27 02:11:24 +0300

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'XDV'
+version '1.0.0'
+description 'En kolay şekilde, Birden fazla modlu aracı tek eklentide birleştirilmek'
+
+files {
+    'ayar/**/*.meta',
+}
+
+data_file 'HANDLING_FILE' 'data/**/**/handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/**/vehiclelayouts_formula.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/**/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/**/carvariations.meta'
+data_file 'CARCOLS_FILE' 'data/**/**/carcols.meta'
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/f1/vehiclelayouts_formula.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'data/f22a/contentunlocks.meta'
+data_file 'WEAPONINFO_FILE' 'dosya/f22a/f22aweapons.meta'
+data_file 'WEAPON_METADATA_FILE' 'dosya/f22a/weaponarchetypes.meta'
